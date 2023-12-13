8d5c51a5d0e65080dce87e57d1e46bec76f65cfe
xneyder <xneyder@gmail.com>
2023-12-13 13:32:17 -0500

    Fix

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,3 @@
-locals {
-  redshift_route_table_ids = aws_route_table.redshift[*].id
-  public_route_table_ids   = aws_route_table.public[*].id
-  private_route_table_ids  = aws_route_table.private[*].id
-}
-
 ################################################################################
 # VPC
 ################################################################################
