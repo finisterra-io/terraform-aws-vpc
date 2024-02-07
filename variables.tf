1ac50c15f713aa74ec5c2e12e3d45f1e7573b6e4
Daniel J <daniel@finisterra.io>
2024-02-06 22:46:53 -0500

    Update Terraform and AWS provider versions

--- variables.tf
+++ variables.tf
@@ -8,12 +8,6 @@ variable "create_vpc" {
   default     = true
 }
 
-variable "name" {
-  description = "Name to be used on all the resources as identifier"
-  type        = string
-  default     = ""
-}
-
 variable "cidr" {
   description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
   type        = string
@@ -98,12 +92,6 @@ variable "ipv6_cidr_block_network_border_group" {
   default     = null
 }
 
-variable "vpc_tags" {
-  description = "Additional tags for the VPC"
-  type        = map(string)
-  default     = {}
-}
-
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
@@ -179,54 +167,10 @@ variable "public_subnets" {
   default     = {}
 }
 
-
-
-variable "public_subnet_assign_ipv6_address_on_creation" {
-  description = "Specify true to indicate that network interfaces created in the specified subnet should be assigned an IPv6 address. Default is `false`"
-  type        = bool
-  default     = false
-}
-
-variable "public_subnet_enable_dns64" {
-  description = "Indicates whether DNS queries made to the Amazon-provided DNS Resolver in this subnet should return synthetic IPv6 addresses for IPv4-only destinations. Default: `true`"
-  type        = bool
-  default     = true
-}
-
-variable "public_subnet_enable_resource_name_dns_aaaa_record_on_launch" {
-  description = "Indicates whether to respond to DNS queries for instance hostnames with DNS AAAA records. Default: `true`"
-  type        = bool
-  default     = true
-}
-
-variable "public_subnet_enable_resource_name_dns_a_record_on_launch" {
-  description = "Indicates whether to respond to DNS queries for instance hostnames with DNS A records. Default: `false`"
-  type        = bool
-  default     = false
-}
-
 variable "public_subnet_ipv6_prefixes" {
   description = "Assigns IPv6 public subnet id based on the Amazon provided /56 prefix base 10 integer (0-256). Must be of equal length to the corresponding IPv4 subnet list"
   type        = list(string)
   default     = []
-}
-
-variable "public_subnet_ipv6_native" {
-  description = "Indicates whether to create an IPv6-only subnet. Default: `false`"
-  type        = bool
-  default     = false
-}
-
-variable "map_public_ip_on_launch" {
-  description = "Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is `false`"
-  type        = bool
-  default     = false
-}
-
-variable "public_subnet_private_dns_hostname_type_on_launch" {
-  description = "The type of hostnames to assign to instances in the subnet at launch. For IPv6-only subnets, an instance DNS name must be based on the instance ID. For dual-stack and IPv4-only subnets, you can specify whether DNS names use the instance IPv4 address or the instance ID. Valid values: `ip-name`, `resource-name`"
-  type        = string
-  default     = null
 }
 
 variable "public_route_tables" {
@@ -239,64 +183,16 @@ variable "public_route_tables" {
 # Private Subnets
 ################################################################################
 
-# variable "private_subnets" {
-#   description = "A list of private subnets inside the VPC"
-#   type        = list(string)
-#   default     = []
-# }
-
 variable "private_subnets" {
   description = "Information about the private subnets to be created"
   type        = any
   default     = {}
 }
 
-variable "private_subnet_assign_ipv6_address_on_creation" {
-  description = "Specify true to indicate that network interfaces created in the specified subnet should be assigned an IPv6 address. Default is `false`"
-  type        = bool
-  default     = false
-}
-
-variable "private_subnet_enable_dns64" {
-  description = "Indicates whether DNS queries made to the Amazon-provided DNS Resolver in this subnet should return synthetic IPv6 addresses for IPv4-only destinations. Default: `true`"
-  type        = bool
-  default     = true
-}
-
-variable "private_subnet_enable_resource_name_dns_aaaa_record_on_launch" {
-  description = "Indicates whether to respond to DNS queries for instance hostnames with DNS AAAA records. Default: `true`"
-  type        = bool
-  default     = true
-}
-
-variable "private_subnet_enable_resource_name_dns_a_record_on_launch" {
-  description = "Indicates whether to respond to DNS queries for instance hostnames with DNS A records. Default: `false`"
-  type        = bool
-  default     = false
-}
-
 variable "private_subnet_ipv6_prefixes" {
   description = "Assigns IPv6 private subnet id based on the Amazon provided /56 prefix base 10 integer (0-256). Must be of equal length to the corresponding IPv4 subnet list"
   type        = list(string)
   default     = []
-}
-
-variable "private_subnet_ipv6_native" {
-  description = "Indicates whether to create an IPv6-only subnet. Default: `false`"
-  type        = bool
-  default     = false
-}
-
-variable "private_subnet_private_dns_hostname_type_on_launch" {
-  description = "The type of hostnames to assign to instances in the subnet at launch. For IPv6-only subnets, an instance DNS name must be based on the instance ID. For dual-stack and IPv4-only subnets, you can specify whether DNS names use the instance IPv4 address or the instance ID. Valid values: `ip-name`, `resource-name`"
-  type        = string
-  default     = null
-}
-
-variable "private_subnet_suffix" {
-  description = "Suffix to append to private subnets name"
-  type        = string
-  default     = "private"
 }
 
 variable "private_route_tables" {
@@ -385,13 +281,6 @@ variable "igw_tags" {
 ################################################################################
 # NAT Gateway
 ################################################################################
-
-variable "enable_nat_gateway" {
-  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
-  type        = bool
-  default     = false
-}
-
 variable "nat_gateways" {
   description = "A map of maps describing what NAT Gateways to create for each availability zone"
   type        = map(any)
@@ -401,37 +290,6 @@ variable "nat_gateways" {
 ################################################################################
 # Default VPC
 ################################################################################
-
-variable "manage_default_vpc" {
-  description = "Should be true to adopt and manage Default VPC"
-  type        = bool
-  default     = false
-}
-
-variable "default_vpc_name" {
-  description = "Name to be used on the Default VPC"
-  type        = string
-  default     = null
-}
-
-variable "default_vpc_enable_dns_support" {
-  description = "Should be true to enable DNS support in the Default VPC"
-  type        = bool
-  default     = true
-}
-
-variable "default_vpc_enable_dns_hostnames" {
-  description = "Should be true to enable DNS hostnames in the Default VPC"
-  type        = bool
-  default     = true
-}
-
-variable "default_vpc_tags" {
-  description = "Additional tags for the Default VPC"
-  type        = map(string)
-  default     = {}
-}
-
 variable "manage_default_security_group" {
   description = "Should be true to adopt and manage default security group"
   type        = bool
@@ -549,127 +407,10 @@ variable "default_route_table_tags" {
 ################################################################################
 # Flow Log
 ################################################################################
-
-variable "enable_flow_log" {
-  description = "Whether or not to enable VPC Flow Logs"
-  type        = bool
-  default     = false
-}
-
-variable "flow_log_destination_type" {
-  description = "Type of flow log destination. Can be s3 or cloud-watch-logs"
-  type        = string
-  default     = "cloud-watch-logs"
-}
-
 variable "aws_flow_logs" {
   description = "A map of maps describing what flow logs to create for each subnet"
   type        = map(any)
   default     = {}
-}
-
-variable "flow_aws_iam_role_name" {
-  description = "The name of the IAM role to create for VPC Flow Logs"
-  type        = string
-  default     = ""
-}
-
-variable "flow_aws_iam_role_assume_role_policy" {
-  description = "The policy that grants an entity permission to assume the role"
-  type        = string
-  default     = ""
-}
-
-variable "flow_aws_iam_role_permissions_boundary" {
-  description = "The ARN of the policy that is used to set the permissions boundary for the role"
-  type        = string
-  default     = ""
-}
-
-variable "flow_aws_iam_role_description" {
-  description = "The description of the role"
-  type        = string
-  default     = ""
-}
-
-variable "flow_aws_iam_role_path" {
-  description = "The path to the role"
-  type        = string
-  default     = ""
-}
-
-variable "flow_aws_iam_role_tags" {
-  description = "A map of tags to assign to the IAM role"
-  type        = map(string)
-  default     = {}
-}
-
-################################################################################
-# Flow Log CloudWatch
-################################################################################
-
-variable "create_flow_log_cloudwatch_log_group" {
-  description = "Whether to create CloudWatch log group for VPC Flow Logs"
-  type        = bool
-  default     = false
-}
-
-variable "create_flow_log_cloudwatch_iam_role" {
-  description = "Whether to create IAM role for VPC Flow Logs"
-  type        = bool
-  default     = false
-}
-
-variable "flow_log_cloudwatch_log_group_retention_in_days" {
-  description = "Specifies the number of days you want to retain log events in the specified log group for VPC flow logs"
-  type        = number
-  default     = null
-}
-
-variable "flow_log_cloudwatch_log_group_kms_key_id" {
-  description = "The ARN of the KMS Key to use when encrypting log data for VPC flow logs"
-  type        = string
-  default     = null
-}
-
-
-
-variable "flow_iam_policy_name" {
-  description = "The name of the IAM policy to create for VPC Flow Logs"
-  type        = string
-  default     = ""
-}
-variable "flow_iam_policy_description" {
-  description = "The description of the IAM policy"
-  type        = string
-  default     = ""
-}
-variable "flow_iam_policy_document" {
-  description = "The policy document of the IAM policy"
-  type        = string
-  default     = ""
-}
-variable "flow_iam_policy_path" {
-  description = "The path to the IAM policy"
-  type        = string
-  default     = ""
-}
-variable "flow_policy_tags" {
-  description = "A map of tags to assign to the IAM policy"
-  type        = map(string)
-  default     = {}
-}
-
-variable "flow_log_cloudwatch_log_group_tags" {
-  description = "A map of tags to assign to the CloudWatch log group"
-  type        = map(string)
-  default     = {}
-}
-
-variable "flow_log_cloudwatch_log_group_name" {
-  description = "The name of the CloudWatch log group for VPC Flow Logs"
-  type        = string
-  default     = ""
 }
 
 variable "dhcp_options_id" {
